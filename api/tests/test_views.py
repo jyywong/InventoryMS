3d9f67fb8caf22035b01d79ce45c00183707a49d
jyywong <wong.jonathan1@gmail.com>
2021-04-04 15:29:35 -0400

    Finished api tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -145,3 +145,141 @@ class APIItemListTests(APIUserAndLabSetUpTestCase):
                 "quantity" : 5
         },)
         self.assertEqual(Item.objects.count(), 2)
+class APIItemDetailTests(APIUserAndLabSetUpTestCase):
+    def setUp(self):
+        super().setUp()
+        self.inv = Inventory.objects.create(
+            lab = self.lab,
+            name = 'Test Inv'
+        )
+        self.item = Item.objects.create(
+            inventory = self.inv,
+            name = "testitem",
+            manufacturer = 'testman',
+            e_date = date.today(),
+            quantity = 5
+        )
+        self.url = reverse('api_item_detail', kwargs={'pk': self.item.id})
+        self.response = self.client.get(self.url)
+    def test_api_item_detail_login_required(self):
+        self.assertEqual(self.response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_api_item_detail_logged_in_no_permission(self):
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_api_item_detail_logged_in_has_permission(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+    def test_api_item_detail_has_correct_data(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.data['name'], 'testitem')
+    def test_api_item_detail_can_patch_succesfully(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.patch(self.url, {
+            "quantity" : 3
+        })
+        self.assertEqual(self.response.data['quantity'], 3)
+    def test_api_item_detail_succesful_patch_creates_item_log(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.patch(self.url, {
+            "quantity" : 3
+        })
+        self.assertEqual(Item_Change_Log.objects.count(), 1)
+    def test_api_item_detail_succesful_patch_creates_correct_item_log(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.patch(self.url, {
+            "quantity" : 3
+        })
+        self.assertEqual(Item_Change_Log.objects.first().quantity, 2)
+    def test_api_item_detail_can_delete_succesfully(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.delete(self.url)
+        self.assertEqual(Item.objects.count(), 0)
+
+class APIItemLogListTests(APIUserAndLabSetUpTestCase):
+    def setUp(self):
+        super().setUp()
+        self.inv = Inventory.objects.create(
+            lab = self.lab,
+            name = 'Test Inv'
+        )
+        self.item = Item.objects.create(
+            inventory = self.inv,
+            name = "testitem",
+            manufacturer = 'testman',
+            e_date = date.today(),
+            quantity = 5
+        )
+        self.itemlog = Item_Change_Log.objects.create(
+            action = 'Add',
+            item = self.item,
+            user = self.user,
+            quantity = 2
+        )
+        self.url = reverse('api_item_change_log', kwargs={'item_pk': self.item.id})
+        self.response = self.client.get(self.url)
+    def test_api_item_log_list_login_required(self):
+        self.assertEqual(self.response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_api_item_log_list_logged_in_no_permission(self):
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_api_item_log_list_logged_in_has_permission(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+    def test_api_item_log_list_has_correct_data(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.data[0]['quantity'], 2)
+
+class APIItemOrderTests(APIUserAndLabSetUpTestCase):
+    def setUp(self):
+        super().setUp()
+        self.inv = Inventory.objects.create(
+            lab = self.lab,
+            name = 'Test Inv'
+        )
+        self.item = Item.objects.create(
+            inventory = self.inv,
+            name = "testitem",
+            manufacturer = 'testman',
+            e_date = date.today(),
+            quantity = 5
+        )
+        self.order = Item_order.objects.create(
+            item = self.item,
+            status = 'Pending',
+            user = self.user, 
+            quantity = 3,
+            needed_by = date.today(),
+            notes = 'hello'
+        )
+        self.url = reverse('api_item_order', kwargs={'lab_pk': self.lab.id})
+        self.response = self.client.get(self.url)
+    def test_api_item_order_login_required(self):
+        self.assertEqual(self.response.status_code, status.HTTP_401_UNAUTHORIZED)
+    def test_api_item_order_logged_in_no_permission(self):
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_api_item_order_logged_in_has_permission(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.status_code, status.HTTP_200_OK)
+    def test_api_item_log_list_has_correct_data(self):
+        self.lab.members.add(self.user)
+        self.client.force_authenticate(self.user)
+        self.response = self.client.get(self.url)
+        self.assertEqual(self.response.data[0]['quantity'], 3)
